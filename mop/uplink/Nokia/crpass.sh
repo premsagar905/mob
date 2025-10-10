925ca8f3db92914b43532d661b8cda4de483504b
Your Name <you@example.com>
2025-10-10 11:42:01 +0530

    updated

---
+++ mop/uplink/Nokia/crpass.sh
@@ -0,0 +1,5 @@
+find . -name ring_closer_rollback.txt
+find . -name pass_through_rollback.txt
+cp ring_closer_rollback.txt pass_through_rollback.txt
+chmod 777 pass_through_rollback.txt
+find . -name pass_through_rollback.txt
